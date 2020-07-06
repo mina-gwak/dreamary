87c56e7883f9a958760474dd371b8d82fa91125f
mina-gwak <mina.gwak@kakao.com>
2020-07-07 02:25:00 +0900

    Detail Page Added / Home.html show Profile Card

--- page/models.py
+++ page/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Designer(models.Model):
-  Image = models.ImageField(upload_to="images/")
+  image = models.ImageField(upload_to="images/")
   name = models.CharField(max_length=50)
   address = models.CharField(max_length=255)
   description = models.TextField()
